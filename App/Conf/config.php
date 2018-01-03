0ba6b63f7add2c59df6f998b41b92e37bdb36041
huyi <2468818839@qq.com>
2018-01-04 00:00:48 +0800

    over banner section

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -16,7 +16,7 @@ $b = array(
 
     /* URL配置 */
     'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
-    'URL_MODEL'            => 3, //URL模式
+    'URL_MODEL'            => 0, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 
@@ -28,7 +28,7 @@ $b = array(
     'DB_HOST'              => '127.0.0.1', // 服务器地址
     'DB_NAME'              => 'easycms', // 数据库名
     'DB_USER'              => 'root', // 用户名
-    'DB_PWD'               => '',  // 密码
+    'DB_PWD'               => 'aa',  // 密码
     'DB_PORT'              => '3306', // 端口
     'DB_PREFIX'            => 'easy_', // 数据库表前缀
 
--- App/Modules/Index/Action/IndexAction.class.php
+++ App/Modules/Index/Action/IndexAction.class.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
-* 前台首页
-* @author  <[c@easycms.cc]>
-*/
-class IndexAction extends CommonAction
-{
+ * 前台首页
+ * @author  <[c@easycms.cc]>
+ */
+class IndexAction extends CommonAction{
     public $img;
     public $title;
     public $desc;
+
     //news index
     public function index(){
-        $url = "http://www.ncnews.com.cn/xwzx/ncxw/"; //nanchang  news  station
+        $url    = "http://www.ncnews.com.cn/xwzx/ncxw"; //nanchang  news  station
         $domain = "http://www.ncnews.com.cn";
-        $con = file_get_contents($url);
+        $con    = file_get_contents($url);
         //p(htmlspecialchars($con));
         //匹配头条
         $pattern = '/<div class=\"topnewslist\".*?>.*?<\/div>/ismu';
@@ -23,51 +24,95 @@ class IndexAction extends CommonAction
         //match images
         $pat1 = '/<a href=\"(.*?)\" target=\"_blank\" class=\"pic\">(.*?)<\/a>/iu';
         preg_match_all($pat1, $arr[0], $images);
-        $img_href[] = $images[1];
+        $img_href = $images[1];   //1
         //imgages  url  address
         $imgs = array();
         foreach($images[2] as $img_v){
             $pat = '/<img .*? data-original=\"(.*?)\".*?>/iu';
-            $pat = '/\"http:\/\/(.*)[\.jpg|\.png]\"/is';
+            $pat = '/http:\/\/(.*)(\.jpg|\.png)\"/isu';
             preg_match_all($pat, $img_v, $img);
-            p(parse_url($img[0][0]));
-            $imgs[] = $img[0][0];
+            $imgs[] = rtrim($img[0][0], '"');   //2
         }
-        p($imgs);
-        foreach($arr[1] as $id => $v){
-            echo "<a href=index2.php?url=$v>" . $arr[2][$id] . "</a>----来源：http://news.sina.com.cn/<br>";
+
+        $pat2 = '/<h3><a href=\"(.*?)\" target=\"_blank\">(.*?)<\/a><\/h3>/';
+        preg_match_all($pat2, $arr[0], $titles);
+        $title_href = array();
+        foreach($titles[1] as $title_v){
+            $title_v      = ltrim($title_v, '.');
+            $title_href[] = $url . $title_v;      //3
         }
+        $title = $titles[2];                   //4
+
+        $pat3 = '/<p>(.*?)<\/p>/';
+        preg_match_all($pat3, $arr[0], $desc);
+        $desc = $desc[1];                       //5
+
+        $data = array();
+        //count($title)
+        for($i = 1; $i < 4; $i++){
+            $data[$i]['img_href']   = $img_href[$i];
+            $data[$i]['imgs']       = $imgs[$i];
+            $data[$i]['title_href'] = $title_href[$i];
+            $data[$i]['title']      = $title[$i];
+            $data[$i]['desc']       = $desc[$i];
+            $data[$i]['twnc']       = "头条南昌";
+            $time                   = pathinfo($img_href[$i])['filename'];
+            $time_arr               = explode('_', $time);
+            $data[$i]['time']       = date("Y-m-d", strtotime(ltrim($time_arr[0], 't')));
+            $data[$i]['idss']       = "message" . $i;
+        }
+
+        $time     = pathinfo($img_href[0])['filename'];
+        $time_arr = explode('_', $time);
+        $one_data = array(
+            'img_href'   => $img_href[0],
+            'imgs'       => $imgs[0],
+            'title_href' => $title_href[0],
+            'title'      => $title[0],
+            'desc'       => $desc[0],
+            'twnc'       => "头条南昌",
+            'time'       => date("Y-m-d", strtotime(ltrim($time_arr[0], 't')))
+        );
+        //p($data);
+        $this->assign('one_data', $one_data);
+        $this->assign('data', $data);
+        $this->assign('datas', $data);
+        $this->display('index');
     }
 
-	Public function index2(){
-		$cats=D('Category');
-		$link=M('link');
-		$portrait=M('Member_user');
-		$article=M('article');
-		//首页中部3篇文章推荐展示
-		$mPushArticle=$article->where('isslides=1 and islock=0')->order('rand()')->select();
-		//首页下部4个分类推荐展示
-		$dPushCats = $cats->limit('4')->where('ispush=1 and isverify=1')->relation(true)->select();
-		$approval1=$article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
-		$approval3=$article->where('ispush=1 and islock=0')->order('rand()')->limit('5')->select();
-		$approval2=$article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
-		$links=$link->where("isverify=1")->select();
-		$portraits=$portrait->where('islock=0')->limit('24')->order('user_id desc')->select();
-		//首页中部3篇文章推荐展示
-		$this->assign('modelid1',$mPushArticle);
-		//这个是页面下面的4个板块
-		$this->assign('modelid0',$dPushCats);
-		//按赞的次数进行遍历
-		$this->assign('approval',$approval1);
-		//随机精选5篇文章
-		$this->assign('approval3',$approval3);
-		//评论最多5篇的文章
-		$this->assign('links',$links);
-		//显示首页友情链接
-		$this->assign('portrait',$portraits);
-		//显示首页右侧用户注册头像
-		$this->assign('approval2',$approval2);
-		//显示模板
-		$this->display('index');
-	}
+    public function index_news_detail(){
+        p($_GET);
+    }
+
+    Public function index2(){
+        $cats     = D('Category');
+        $link     = M('link');
+        $portrait = M('Member_user');
+        $article  = M('article');
+        //首页中部3篇文章推荐展示
+        $mPushArticle = $article->where('isslides=1 and islock=0')->order('rand()')->select();
+        //首页下部4个分类推荐展示
+        $dPushCats = $cats->limit('4')->where('ispush=1 and isverify=1')->relation(true)->select();
+        $approval1 = $article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
+        $approval3 = $article->where('ispush=1 and islock=0')->order('rand()')->limit('5')->select();
+        $approval2 = $article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
+        $links     = $link->where("isverify=1")->select();
+        $portraits = $portrait->where('islock=0')->limit('24')->order('user_id desc')->select();
+        //首页中部3篇文章推荐展示
+        $this->assign('modelid1', $mPushArticle);
+        //这个是页面下面的4个板块
+        $this->assign('modelid0', $dPushCats);
+        //按赞的次数进行遍历
+        $this->assign('approval', $approval1);
+        //随机精选5篇文章
+        $this->assign('approval3', $approval3);
+        //评论最多5篇的文章
+        $this->assign('links', $links);
+        //显示首页友情链接
+        $this->assign('portrait', $portraits);
+        //显示首页右侧用户注册头像
+        $this->assign('approval2', $approval2);
+        //显示模板
+        $this->display('index');
+    }
 }
