ce22b7361ee82e6ec77f09be6c6aef5acfe1c58d
huyi@weyee.com <huyi@weyee.com>
2018-01-05 16:49:57 +0800

    dd

--- App/Modules/Index/Action/IndexAction.class.php
+++ App/Modules/Index/Action/IndexAction.class.php
@@ -14,174 +14,38 @@ class IndexAction extends CommonAction{
 
     //news index
     public function index(){
-        $url    = "http://www.ncnews.com.cn/xwzx/ncxw"; //nanchang  news  station
+        //$url1    = "http://www.ncnews.com.cn/xwzx/ncxw"; //nanchang  news  station
+        $url    = "http://www.ncnews.com.cn/xwzx/ncxw/bwzg_rd/"; //本网原创
         $domain = "http://www.ncnews.com.cn";
-        $con    = file_get_contents($url);
-        //p(htmlspecialchars($con));
-        //匹配头条
-        $pattern = '/<div class=\"topnewslist\".*?>.*?<\/div>/ismu';
-        //match banner  新闻
-        $pattern = '/<ul class=\"headnewslist\">(.*?)<\/ul>/ismu';
-        preg_match($pattern, $con, $arr);
-        //p($arr);
-        //match images
-        $pat1 = '/<a href=\"(.*?)\" target=\"_blank\" class=\"pic\">(.*?)<\/a>/iu';
-        preg_match_all($pat1, $arr[0], $images);
-        $img_href = $images[1];   //1
-        //imgages  url  address
-        $imgs = array();
-        foreach($images[2] as $img_v){
-            $pat = '/<img .*? data-original=\"(.*?)\".*?>/iu';
-            $pat = '/http:\/\/(.*)(\.jpg|\.png)\"/isu';
-            preg_match_all($pat, $img_v, $img);
-            $imgs[] = rtrim($img[0][0], '"');   //2
-        }
+        $index_m = new IndexModel();
+        //$res = $index_m -> index($url1);
+        $res = $index_m -> bwzg_rd($url);
 
-        $pat2 = '/<h3><a href=\"(.*?)\" target=\"_blank\">(.*?)<\/a><\/h3>/';
-        preg_match_all($pat2, $arr[0], $titles);
-        $title_href = array();
-
-        foreach($titles[1] as $title_v){
-            $sub = substr($title_v, 0, 1);
-            if($sub == 'h'){
-                $title_href[] = $title_v;      //3
-            }elseif($sub == '.'){
-                $title_v      = ltrim($title_v, '.');
-                $title_href[] = $url . $title_v;      //3
-            }else{
-                $title_v      = ltrim($title_v, '.');
-                $title_href[] = $url . $title_v;      //3
-            }
-        }
-        $title = $titles[2];                   //4
-
-        $pat3 = '/<p>(.*?)<\/p>/';
-        preg_match_all($pat3, $arr[0], $desc);
-        $desc = $desc[1];                       //5
-
-        $data = array();
-        //count($title)
-        for($i = 1; $i < 4; $i++){
-            $data[$i]['img_href']   = $img_href[$i];
-            $data[$i]['imgs']       = $imgs[$i];
-            $data[$i]['title_href'] = $title_href[$i];
-            $data[$i]['title']      = $title[$i];
-            $data[$i]['desc']       = $desc[$i];
-            $data[$i]['twnc']       = "图文南昌";
-            $time                   = pathinfo($img_href[$i])['filename'];
-            $time_arr               = explode('_', $time);
-            $data[$i]['time']       = date("Y-m-d", strtotime(ltrim($time_arr[0], 't')));
-            $data[$i]['idss']       = "message" . $i;
-        }
-
-        $time     = pathinfo($img_href[0])['filename'];
-        $time_arr = explode('_', $time);
-        $one_data = array(
-            'img_href'   => $img_href[0],
-            'imgs'       => $imgs[0],
-            'title_href' => $title_href[0],
-            'title'      => $title[0],
-            'desc'       => $desc[0],
-            'twnc'       => "图文南昌",
-            'time'       => date("Y-m-d", strtotime(ltrim($time_arr[0], 't')))
-        );
-        //p($data);
-        $this->assign('one_data', $one_data);
-        $this->assign('data', $data);
-        $this->assign('datas', $data);
+        $this->assign('one_data', $res['one_data']);
+        $this->assign('data', $res['data']);
+        $this->assign('datas', $res['data']);
         $this->display('index');
     }
 
     public function index_news_detail(){
         $news_content_url = $_GET['url'];
-        $host = pathinfo($news_content_url);
-        $img_url_ = $host['dirname'];
+        $index_m = new IndexModel();
+        $arr = $index_m ->bwzg_rd_detail($news_content_url);
 
-        $news_detail      = requests::get($news_content_url);
-        $news_content     = selector::select($news_detail, "//div[contains(@class,'newsread')]");
-        p($news_content);
-        /****类别内容****/
-        //$news_top = requests::get($img_url_);
-        //$pos = strrpos($img_url_,'/');
-        //$domain_url_  =  substr($img_url_,0,$pos);
-        //$news_cate_content = requests::get($domain_url_);
-        /****类别内容****/
-
-        //xpath
-        $one_news_title = selector::select($news_detail, "//div[contains(@class,'read')]//h3"); //1
-        $one_news_top   = selector::select($news_detail, "//div[contains(@class,'newsinfo')]");
-        $one_news_top   = selector::remove($one_news_top, "//i[contains(@id,'fontsize')]");
-        $one_news_time  = trim($one_news_top);      //2
-
-        $one_news_imgs_tags = selector::select($news_content, "//div[contains(@align,'center')]");
-        $pattern            = '/<img[^>]*.?>/i';
-        if(!empty($one_news_imgs_tags)){
-            foreach($one_news_imgs_tags as $k => $tag){
-                if(preg_match($pattern, $tag)){
-                    $one_news_imgs_tags[$k] = $img_url_.ltrim(selector::select($tag, "//img"),'.'); //3
-                }
-            }
-        }
-
-        $p = "/<img[^>]*src[=\"\'\s]+([^\"\']*)[\"\']?[^>]*>((?:(?!<img\b)[\s\S])*)/i";
-        //$news_content= '<div align="left">34534<p>sdfas</p></div>';
-        $p                = '/<div[^>]*?align=\"left\"*?>(.*?)(<p>(.*?)<\/p>)*<\/div>/ism';
-        $p                = '/<div[^>]*?align=\"left\"*?>(.*?)(<p[^>]*?>(.*?)<\/p>.*?)?<\/div>/ism';
-        //$one_news_content = selector::select($news_content, "//div[contains(@align,'left')]");
-        $content          = selector::select($news_content, $p, 'regex');
-        $content_title    = isset($content[0]) ? trim($content[0]) : '';
-        $content_main     = isset($content[2]) ? trim($content[2]) : '';
-
-        //获取banner
-        $banner = array();
-       foreach($one_news_imgs_tags as  $imgs_tag){
-            $one = substr($imgs_tag,0,1);
-            if($one == 'h'){
-                $banner[] = $imgs_tag;
-            }
-       }
-
-
-        $this -> assign('one_news_title',$one_news_title);
-        $this -> assign('one_news_time',$one_news_time);
-        $this -> assign('one_news_imgs_tags',$one_news_imgs_tags);
-        $this -> assign('content_title',$content_title);
-        $this -> assign('content_main',$content_main);
-        $this -> assign('banner',$banner[0]);
-        //p($one_news_imgs_tags);
-
-        $this->display('news');
+        $this -> assign('one_news_title',$arr['one_news_title']);
+        $this -> assign('one_news_time',$arr['one_news_time']);
+        $this -> assign('one_news_imgs_tags',$arr['one_news_imgs_tags']);
+        $this -> assign('content_title',$arr['content_title']);
+        $this -> assign('content_main',$arr['content_main']);
+        $this -> assign('banner',$arr['banner']);
+        $this -> assign('news_tag',$arr['news_tag']);
+        $this-> display('news');
     }
 
-    Public function index2(){
-        $cats     = D('Category');
-        $link     = M('link');
-        $portrait = M('Member_user');
-        $article  = M('article');
-        //首页中部3篇文章推荐展示
-        $mPushArticle = $article->where('isslides=1 and islock=0')->order('rand()')->select();
-        //首页下部4个分类推荐展示
-        $dPushCats = $cats->limit('4')->where('ispush=1 and isverify=1')->relation(true)->select();
-        $approval1 = $article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
-        $approval3 = $article->where('ispush=1 and islock=0')->order('rand()')->limit('5')->select();
-        $approval2 = $article->where('ispush=1 and islock=0')->order('article_id desc')->limit('5')->select();
-        $links     = $link->where("isverify=1")->select();
-        $portraits = $portrait->where('islock=0')->limit('24')->order('user_id desc')->select();
-        //首页中部3篇文章推荐展示
-        $this->assign('modelid1', $mPushArticle);
-        //这个是页面下面的4个板块
-        $this->assign('modelid0', $dPushCats);
-        //按赞的次数进行遍历
-        $this->assign('approval', $approval1);
-        //随机精选5篇文章
-        $this->assign('approval3', $approval3);
-        //评论最多5篇的文章
-        $this->assign('links', $links);
-        //显示首页友情链接
-        $this->assign('portrait', $portraits);
-        //显示首页右侧用户注册头像
-        $this->assign('approval2', $approval2);
-        //显示模板
-        $this->display('index');
+    //循环爬取
+    public function index_spider(){
+        $spider_m = new SpiderModel();
+        $spider_m -> spider1();
     }
+
 }
--- App/Runtime/Cache/Index/efbc238391f0696f1590194633888fe9.php
+++ App/Runtime/Cache/Index/efbc238391f0696f1590194633888fe9.php
@@ -262,51 +262,45 @@
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-9 p0 wrap-headline">
-                        <img src="__IMAGES__/content/article.jpg" alt="img" class="wrap-headline__img">
+                        <img src="<?php echo ($banner); ?>" alt="img" class="wrap-headline__img">
                         <div class="headline clearfix">
                             <div class="headline__data">
-                                <a href="category.html" class="headline__category headline__category_orange">Policy</a>
-                                <p class="headline__category">today, 18:48</p>
+                                <a href="<?php echo U('Index/Index/index/',array('n'=>2));?>" class="headline__category headline__category_orange">首页</a>
+                                <p class="headline__category"><?php echo ($one_news_time); ?></p>
                             </div>
-                            <div class="share">
-                                <ul class="share__list">
-                                    <li class="share__row">
-                                        <a class="share__link share__link_bg-facebook" href="#">
-                                            <i class="bg bg_facebook">&ensp;</i>
-                                        </a>
-                                        <span class="share__number">13</span>
-                                    </li>
-                                    <li class="share__row">
-                                        <a class="share__link share__link_bg-twitter" href="#">
-                                            <i class="bg bg_twitter">&ensp;</i>
-                                        </a>
-                                        <span class="share__number">26</span>
-                                    </li>
-                                    <li class="share__row">
-                                        <a class="share__link share__link_bg-google" href="#">
-                                            <i class="bg bg_google">&ensp;</i>
-                                        </a>
-                                        <span class="share__number">17</span>
-                                    </li>
-                                </ul>
-                            </div>
+                            <!--<div class="share">-->
+                                <!--<ul class="share__list">-->
+                                    <!--<li class="share__row">-->
+                                        <!--<a class="share__link share__link_bg-facebook" href="#">-->
+                                            <!--<i class="bg bg_facebook">&ensp;</i>-->
+                                        <!--</a>-->
+                                        <!--<span class="share__number">13</span>-->
+                                    <!--</li>-->
+                                    <!--<li class="share__row">-->
+                                        <!--<a class="share__link share__link_bg-twitter" href="#">-->
+                                            <!--<i class="bg bg_twitter">&ensp;</i>-->
+                                        <!--</a>-->
+                                        <!--<span class="share__number">26</span>-->
+                                    <!--</li>-->
+                                    <!--<li class="share__row">-->
+                                        <!--<a class="share__link share__link_bg-google" href="#">-->
+                                            <!--<i class="bg bg_google">&ensp;</i>-->
+                                        <!--</a>-->
+                                        <!--<span class="share__number">17</span>-->
+                                    <!--</li>-->
+                                <!--</ul>-->
+                            <!--</div>-->
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-9 article_text">
                         <div class="current">
-                            <h1 class="text-center yel_line current__title">Insiders Guide to Miami</h1>
-                            <p class="current__text">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth,
-                                the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely
-                                painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To
-                                take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying
-                                consequences, or one who avoids a pain that produces no resultant pleasure?</p>
-                            <img src="__IMAGES__/content/article2.jpg" alt="img" class="current__img">
-                            <p class="current__text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
-                                ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-                                adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid
-                                ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
-                            <blockquote class="quote current__quote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</blockquote>
-                            <p class="current__text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            <h1 class="text-center yel_line current__title"><?php echo ($one_news_title); ?></h1>
+                            <?php if($content_title != null): ?><p class="current__text"><?php echo ($content_title); ?></p><?php endif; ?>
+
+                          <?php if($one_news_imgs_tags != null): for($i=0;$i<count($one_news_imgs_tags);$i++){ echo "<img src='".$one_news_imgs_tags[$i]."' alt=\"img\" class=\"current__img\">"; if(isset($news_tag) && !empty($news_tag)){ echo '<blockquote class="quote current__quote">'.$news_tag[$i].'</blockquote>'; } } endif; ?>
+
+                            <?php if($content_main != null): if(is_array($content_main)): foreach($content_main as $key=>$new_main): ?><p class="current__text"><?php echo ($new_main); ?></p><?php endforeach; endif; endif; ?>
+
                         </div>
                     </div>
                     <div class="col-sm-12 col-md-9 tags">
